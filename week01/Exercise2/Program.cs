9b07c42872bbc1153771f4667852a4c93dceeaa9
Wildstar7 <catswildstarjapan7@gmail.com>
2025-09-04 21:45:13 -0700

    First 3 W01 Exercises

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,65 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.Write("What is your grade percentage? ");
+        string stringGradePercentage = Console.ReadLine();
+        int gradePercentage = int.Parse(stringGradePercentage);
+
+        string letterGrade = "";
+        if (gradePercentage >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (gradePercentage >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (gradePercentage >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (gradePercentage >= 60)
+        {
+            letterGrade = "D";
+        }
+        else
+        {
+            letterGrade = "F";
+        }
+
+
+        int lastDigit = gradePercentage % 10;
+        string gradeSign = "";
+
+        if (gradePercentage >= 100)
+        {
+            gradeSign = "+";
+        }
+        else if (lastDigit >= 7)
+        {
+            gradeSign = "+";
+        }
+        else if (lastDigit < 3)
+        {
+            gradeSign = "-";
+        }
+        else
+        {
+            gradeSign = "";
+        }
+
+        string fullLetterGrade = $"{letterGrade}{gradeSign}";
+
+        if (fullLetterGrade == "A+")
+        {
+            fullLetterGrade = "A";
+        }
+        else if (fullLetterGrade == "F+" || fullLetterGrade == "F-")
+        {
+            fullLetterGrade = "F";
+        }
+        else {}
+
+        Console.WriteLine($"You got a {fullLetterGrade}");
     }
 }
--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -4,6 +4,42 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+        string playAgain = "no";
+        while (playAgain == "yes")
+        {
+            if (playAgain == "yes")
+            {
+                playAgain = "no";
+            }
+            Random randomGenerator = new Random();
+            int magicNumber = randomGenerator.Next(1, 101);
+
+            int userGuess = 0;
+            int numGuesses = 0;
+            while (userGuess != magicNumber)
+            {
+                Console.Write("What is your guess? ");
+                string stringUserGuess = Console.ReadLine();
+                userGuess = int.Parse(stringUserGuess);
+                numGuesses++;
+
+                if (userGuess > magicNumber)
+                {
+                    Console.WriteLine("Lower");
+                }
+                else if (userGuess < magicNumber)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else if (userGuess == magicNumber)
+                {
+                    Console.WriteLine("You guessed it!");
+                    Console.WriteLine($"You got it in {numGuesses} guesses.");
+                    Console.WriteLine("Do you want to play again? ");
+                    playAgain = Console.ReadLine();
+                }
+                else { }
+            }
+        }
     }
 }
