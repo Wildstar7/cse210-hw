ffd4bce077e2984fdbf5b683efc72a9aeedee35b
Wildstar7 <catswildstarjapan7@gmail.com>
2025-10-03 20:36:22 -0700

    W03 Learning Activity

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,56 @@
+public class Fraction
+{
+    // Variables
+    private int _top;
+    private int _bottom;
+
+    // Constructors
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    // Methods
+    public void SetTop(int top)
+    {
+        _top = top;
+    }
+
+    public void SetBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+
+    public int GetTop()
+    {
+        return _top;
+    }
+
+    public int GetBottom()
+    {
+        return _bottom;
+    }
+
+    public string GetFractionString()
+    {
+        return $"{_top}/{_bottom}";
+    }
+
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
--- week03/Fractions/Program.cs
+++ week03/Fractions/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Fractions Project.");
+        Fraction sample = new Fraction();
+
+        Console.WriteLine(sample.GetFractionString());
+        Console.WriteLine(sample.GetDecimalValue());
+
+        sample.SetTop(5);
+        Console.WriteLine(sample.GetFractionString());
+        Console.WriteLine(sample.GetDecimalValue());
+
+        sample.SetTop(3);
+        sample.SetBottom(4);
+        Console.WriteLine(sample.GetFractionString());
+        Console.WriteLine(sample.GetDecimalValue());
+
+        sample.SetTop(1);
+        sample.SetBottom(3);
+        Console.WriteLine(sample.GetFractionString());
+        Console.WriteLine(sample.GetDecimalValue());
     }
 }
