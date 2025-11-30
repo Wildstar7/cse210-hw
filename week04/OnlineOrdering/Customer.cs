7aed1800549363757f073d33a8a4e4a51aea117f
Wildstar7 <catswildstarjapan7@gmail.com>
2025-11-29 17:07:03 -0800

    Online Ordering Finishing Touches

--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -1,8 +1,29 @@
 public class Customer
 {
     // Variables
+    private string _name;
+    private Address _address;
 
     // Constructors
+    public Customer(string name, Address address)
+    {
+        _name = name;
+        _address = address;
+    }
 
     // Methods
+    public string GetName()
+    {
+        return _name;
+    }
+
+    public string GetAddress()
+    {
+        return _address.FullAddress();
+    }
+
+    public bool CustomerInUSA()
+    {
+        return _address.InUSA();
+    }
 }
--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -1,8 +1,33 @@
 public class Product
 {
     // Variables
+    private string _product;
+    private int _productID;
+    private float _productPrice;
+    private int _productQuantity;
 
     // Constructors
+    public Product(string product, int productID, float productPrice, int quantity)
+    {
+        _product = product;
+        _productID = productID;
+        _productPrice = productPrice;
+        _productQuantity = quantity;
+    }
 
     // Methods
+    public string GetProductName()
+    {
+        return _product;
+    }
+
+    public int GetProductID()
+    {
+        return _productID;
+    }
+
+    public float ProductTotalPrice()
+    {
+        return _productQuantity * _productPrice;
+    }
 }
