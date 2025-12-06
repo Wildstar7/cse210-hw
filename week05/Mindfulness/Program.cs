ea048c881d0c761920d154af95871a5c68032fed
Wildstar7 <catswildstarjapan7@gmail.com>
2025-12-05 16:00:32 -0800

    Started menu loop for mindfulness program

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -4,6 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Mindfulness Project.");
+        int menuLoop = 0;
+        while (menuLoop == 0)
+        {
+            Console.WriteLine("Please select an activity:/n");
+            Console.WriteLine("1) Breathing Activity");
+            Console.WriteLine("2) Reflection Activity");
+            Console.WriteLine("3) Listing Activity");
+            Console.WriteLine("4) Quit");
+
+            Console.Write("/nWhat is your selection? ");
+            string userSelection = Console.ReadLine();
+            int userSelectionInt = int.Parse(userSelection);
+
+            switch (userSelectionInt)
+            {
+                case 1:
+                    Console.WriteLine("Breathing Activity");
+                    break;
+
+                case 2:
+                    Console.WriteLine("Reflection Activity");
+                    break;
+
+                case 3:
+                    Console.WriteLine("Listing Activity");
+                    break;
+
+                case 4:
+                    Console.WriteLine("Quitting Time!");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid Option!!!!");
+                    break;
+            }
+        }
     }
 }
